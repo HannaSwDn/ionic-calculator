16b74c9da228e6ed0d8d4eb7a80739cb0523a437
hanna_nyman <hannanyman.sk@gmail.com>
2018-12-28 16:26:56 +0100

    Adds reset function which clears the screen of the calculator

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -9,9 +9,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class CalculatorPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
   }
 
   // add function for the power button
+  reset() {
+    let screen: HTMLElement = document.querySelector('.screen')
+    let second: number = 0
+
+    setInterval(function(){
+      second++
+      if (second % 2 == 0) {
+        screen.innerHTML = ''
+      } else if (second % 2 != 0) {
+        screen.innerHTML = '.'
+      }
+    }, 500);
+  }
 
   // add calculate function
 
